4d803feb5990518dc43e4fa243b5f049be702e90
Jared <jaredgomez0812@gmail.com>
2025-10-17 23:19:39 -0400

    deploying to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,20 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
       theme: "material-theme-ocean",
     },
   },
+
+  adapter: vercel(),
 });
